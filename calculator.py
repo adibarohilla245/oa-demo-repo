95bb315d6ffacfb5986dc6966dbca4a3ab13a7b6
Open Architect <openarchitect@gmail.com>
2026-09-14 15:34:28 +0500

    Apply diff to multiple files

--- calculator.py
+++ calculator.py
@@ -1,2 +1,7 @@
-   def add(a, b):
-       return a + b
+def add(a, b):
+    return a + b
+
+def average(numbers):
+    if not numbers:
+        raise ValueError('The list cannot be empty')
+    return sum(numbers) / len(numbers)
